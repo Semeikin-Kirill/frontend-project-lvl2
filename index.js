97bf6d7de794660f423a0edd545858764dd6c6fd
Kirill_Semeykin <semeikinkirill21@gmail.com>
2021-06-24 10:26:04 +0300

    fix

--- index.js
+++ index.js
@@ -2,35 +2,39 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
-export default (filePath1, filePath2) => {
-  if (
-    path.extname(filePath1) !== '.json'
-    || path.extname(filePath2) !== '.json'
-  ) {
+const getPath = (directory, filePath) => path.resolve(directory, filePath);
+
+const readFile = (file) => fs.readFileSync(file);
+
+const parseJson = (directory, filePath) => {
+  if (path.extname(filePath) !== '.json') {
     throw new Error('Invalid format');
   }
+  const file = readFile(getPath(directory, filePath));
+  return JSON.parse(file);
+};
+
+export default (filePath1, filePath2) => {
   const result = [];
   const currentDirectory = process.cwd();
-  const correctPathFile1 = path.resolve(currentDirectory, filePath1);
-  const correctPathFile2 = path.resolve(currentDirectory, filePath2);
-  const file1 = fs.readFileSync(correctPathFile1);
-  const file2 = fs.readFileSync(correctPathFile2);
-  const obj1 = JSON.parse(file1);
-  const obj2 = JSON.parse(file2);
-  const keys1 = _.keys(obj1);
-  const keys2 = _.keys(obj2);
-  const keys = _.union(keys1, keys2);
+  const objectFile1 = parseJson(currentDirectory, filePath1);
+  const objectFile2 = parseJson(currentDirectory, filePath2);
+  const commomObject = { ...objectFile1, ...objectFile2 };
+  const keys = Object.keys(commomObject);
   _.sortBy(keys).forEach((key) => {
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        result.push(`  ${key}: ${obj1[key]}`);
+    if (_.has(objectFile1, key) && _.has(objectFile2, key)) {
+      if (objectFile1[key] === objectFile2[key]) {
+        result.push(`  ${key}: ${objectFile1[key]}`);
       } else {
-        result.push(`- ${key}: ${obj1[key]}`, `+ ${key}: ${obj2[key]}`);
+        result.push(
+          `- ${key}: ${objectFile1[key]}`,
+          `+ ${key}: ${objectFile2[key]}`,
+        );
       }
     } else {
-      const string = _.has(obj1, key)
-        ? `- ${key}: ${obj1[key]}`
-        : `+ ${key}: ${obj2[key]}`;
+      const string = _.has(objectFile1, key)
+        ? `- ${key}: ${objectFile1[key]}`
+        : `+ ${key}: ${objectFile2[key]}`;
       result.push(string);
     }
   });
