855ed8714a16b3c7d2535876d30011333515c254
Kirill_Semeykin <semeikinkirill21@gmail.com>
2021-08-04 08:56:01 +0300

    fix mutation

--- src/createTreeDiff.js
+++ src/createTreeDiff.js
@@ -7,22 +7,23 @@ const createTreeDiff = (value1, value2) => {
 
   const keys = Object.keys({ ...value1, ...value2 });
   const result = _.sortBy(keys).reduce((acc, key) => {
+    const accCopy = { ...acc };
     if (_.has(value1, key) && _.has(value2, key)) {
       if (
         (!_.isObject(value1[key]) || !_.isObject(value2[key]))
         && value1[key] !== value2[key]
       ) {
-        acc[`- ${key}`] = createTreeDiff(value1[key], value1[key]);
-        acc[`+ ${key}`] = createTreeDiff(value2[key], value2[key]);
+        accCopy[`- ${key}`] = createTreeDiff(value1[key], value1[key]);
+        accCopy[`+ ${key}`] = createTreeDiff(value2[key], value2[key]);
       } else {
-        acc[`  ${key}`] = createTreeDiff(value1[key], value2[key]);
+        accCopy[`  ${key}`] = createTreeDiff(value1[key], value2[key]);
       }
     } else if (_.has(value1, key)) {
-      acc[`- ${key}`] = createTreeDiff(value1[key], value1[key]);
+      accCopy[`- ${key}`] = createTreeDiff(value1[key], value1[key]);
     } else {
-      acc[`+ ${key}`] = createTreeDiff(value2[key], value2[key]);
+      accCopy[`+ ${key}`] = createTreeDiff(value2[key], value2[key]);
     }
-    return acc;
+    return accCopy;
   }, {});
 
   return result;
