34b756170fd9c76394ed5f6be82926ddeb58f5b2
Kirill_Semeykin <semeikinkirill21@gmail.com>
2021-08-04 12:04:50 +0300

    fix mutation

--- src/createTreeDiff.js
+++ src/createTreeDiff.js
@@ -1,31 +1,37 @@
 import _ from 'lodash';
 
-const createTreeDiff = (value1, value2) => {
-  if (!_.isObject(value1) && !_.isObject(value2)) {
-    return value1;
+const isEqual = (value1, value2) => {
+  if (value1 === value2) {
+    return true;
   }
-
-  const keys = Object.keys({ ...value1, ...value2 });
-  const result = _.sortBy(keys).reduce((acc, key) => {
-    const accCopy = { ...acc };
-    if (_.has(value1, key) && _.has(value2, key)) {
-      if (
-        (!_.isObject(value1[key]) || !_.isObject(value2[key]))
-        && value1[key] !== value2[key]
-      ) {
-        accCopy[`- ${key}`] = createTreeDiff(value1[key], value1[key]);
-        accCopy[`+ ${key}`] = createTreeDiff(value2[key], value2[key]);
-      } else {
-        accCopy[`  ${key}`] = createTreeDiff(value1[key], value2[key]);
-      }
-    } else if (_.has(value1, key)) {
-      accCopy[`- ${key}`] = createTreeDiff(value1[key], value1[key]);
-    } else {
-      accCopy[`+ ${key}`] = createTreeDiff(value2[key], value2[key]);
-    }
-    return accCopy;
-  }, {});
-
-  return result;
+  if (_.isObject(value1) && _.isObject(value2)) {
+    return true;
+  }
+  return false;
 };
+
+const createTreeDiff = (obj1, obj2) => {
+  if (!_.isObject(obj1) && !_.isObject(obj2)) {
+    return obj2;
+  }
+  const keys = Object.keys({ ...obj1, ...obj2 });
+  return _.sortBy(keys).reduce((tree, key) => {
+    if (_.has(obj2, key)) {
+      if (!_.has(obj1, key)) {
+        const obj = { [`+ ${key}`]: createTreeDiff(obj2[key], obj2[key]) };
+        return { ...tree, ...obj };
+      }
+      if (isEqual(obj1[key], obj2[key])) {
+        const obj = { [`  ${key}`]: createTreeDiff(obj1[key], obj2[key]) };
+        return { ...tree, ...obj };
+      }
+      const value1 = createTreeDiff(obj1[key], obj1[key]);
+      const value2 = createTreeDiff(obj2[key], obj2[key]);
+      const obj = { [`- ${key}`]: value1, [`+ ${key}`]: value2 };
+      return { ...tree, ...obj };
+    }
+    return { ...tree, [`- ${key}`]: createTreeDiff(obj1[key], obj1[key]) };
+  }, {});
+};
+
 export default createTreeDiff;
